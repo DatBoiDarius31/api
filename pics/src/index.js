3d71cb62d9a63254b06baa52b882b36b167289e5
Darius Smith <91650708+DatBoiDarius31@users.noreply.github.com>
2024-01-11 14:00:35 -0600

    started section 5
    not done

---
+++ pics/src/index.js
@@ -0,0 +1,7 @@
+import react from 'react';
+import reactDom from 'react-dom/client';
+import App from './App';
+
+const el = document.getElementById('root');
+const root = reactDom.createRoot(el);
+root.render(<App />);
